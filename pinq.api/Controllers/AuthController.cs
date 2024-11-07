f5799b5faf153fd8646dc423f39dee406feaa8eb
yooud <khijnyak.dima@gmail.com>
2024-11-07 11:56:20 +0200

    add session cache refresh when new session is received

--- pinq.api/Controllers/AuthController.cs
+++ pinq.api/Controllers/AuthController.cs
@@ -3,13 +3,17 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using pinq.api.Models.Dto.Auth;
 using pinq.api.Repository;
+using pinq.api.Services;
 
 namespace pinq.api.Controllers;
 
 [ApiController]
 [Route("auth")]
 [Authorize]
-public class AuthController(IUserRepository userRepository, IUserSessionRepository sessionRepository) : ControllerBase
+public class AuthController(
+    IUserRepository userRepository,
+    IUserSessionRepository sessionRepository,
+    ISessionCacheService sessionCacheService) : ControllerBase
 {
     [HttpPost]
     public async Task<IActionResult> Login([FromBody] LoginDto request)
@@ -23,6 +27,7 @@ public class AuthController(IUserRepository userRepository, IUserSessionReposito
 
         var sessionId = Guid.NewGuid();
         await sessionRepository.UpdateSession(uid, request.FcmToken, sessionId);
+        await sessionCacheService.SetSessionAsync(uid, sessionId.ToString());
         
         Response.Headers["X-Session-Id"] = sessionId.ToString();
         return NoContent();
